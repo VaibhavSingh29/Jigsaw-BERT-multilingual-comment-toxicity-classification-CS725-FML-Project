3f17bf3b774d64055aa3744bd84e4008809000e9
AfrinDange <dangeafrin@gmail.com>
2022-11-25 11:03:27 +0530

    create backend functions

---
+++ website/app/main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify, render_template
+from torch_utils import get_encoding, get_prediction
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # get sentence, tokenize, predict, return prob
+    if request.method == 'POST':
+        if request.params.sentence != "":
+            return jsonify({
+                'error': 'no input'
+            })
+        try:
+            input_ids, attention_mask = get_encoding(request.params.sentence)
+            prob = get_prediction(input_ids, attention_mask)
+            return jsonify({
+                'result': prob.tolist()
+            })
+        except:
+            return jsonify({
+                'error': 'something is wrong :('
+            })
+
+
+if(__name__ == '__main__'):
+    app.run()
