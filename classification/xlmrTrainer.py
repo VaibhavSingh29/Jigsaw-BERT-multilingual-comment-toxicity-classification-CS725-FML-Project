aa7c8c07fcf99f285368465de0f25fa2b40d49e7
AfrinDange <dangeafrin@gmail.com>
2022-11-25 11:55:12 +0530

    train model & perform analysis

--- classification/xlmrTrainer.py
+++ classification/xlmrTrainer.py
@@ -1,16 +1,15 @@
 # imports
 import os
 from tqdm import tqdm
-
+import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader
-
 from transformers import XLMRobertaTokenizer, XLMRobertaForSequenceClassification
 from transformers import get_linear_schedule_with_warmup
 from transformers import logging
-
+from sklearn.utils import class_weight
 logging.set_verbosity_error()
 
 if torch.cuda.is_available():
@@ -89,7 +88,7 @@ class Classifier(nn.Module):
         return output
 
     def train(self, train_loader, hparams):
-        criterion = nn.CrossEntropyLoss()
+        criterion = nn.CrossEntropyLoss(weight=hparams['weights'].to(DEVICE))
         optimizer = torch.optim.Adam(
             self.parameters(),
             lr=hparams['lr'],
@@ -103,7 +102,7 @@ class Classifier(nn.Module):
         )
 
         for epoch in range(hparams['epochs']):
-            for i, (input_ids, attention_mask, labels) in enumerate(train_loader):
+            for i, (input_ids, attention_mask, labels) in enumerate(tqdm(train_loader, desc="minibatches trained on")):
                 input_ids = input_ids.to(DEVICE)
                 attention_mask = attention_mask.to(DEVICE)
                 labels = labels.type(torch.LongTensor).to(DEVICE)
@@ -145,9 +144,17 @@ class Classifier(nn.Module):
         with torch.no_grad():
             input_ids = input_ids.to(DEVICE)
             attention_mask = attention_mask.to(DEVICE)
-            output = self.forward(input_ids, attention_mask)
+            output = self.model(input_ids, attention_mask)
             pred = torch.argmax(output.logits, dim=1)
             return pred
+
+    def predict_prob(self, input_ids, attention_mask):
+        with torch.no_grad():
+            input_ids = input_ids.to(DEVICE)
+            attention_mask = attention_mask.to(DEVICE)
+            output = self.model(input_ids, attention_mask)
+            prob = torch.softmax(output.logits, dim=1)
+            return prob
 
 
 def save_model(model, checkpoint, accuracy):
@@ -160,17 +167,32 @@ def main():
 
     # hyperparameters
     hparams = {}
-    hparams['epochs'] = 4
-    hparams['batch_size'] = 1
-    hparams['lr'] = 2e-5
+    hparams['epochs'] = 2
+    hparams['batch_size'] = 32
+    hparams['lr'] = 1e-6
     hparams['beta_1'] = 0.9
     hparams['beta_2'] = 0.999
     hparams['token_length'] = 200
 
     checkpoint = "xlm-roberta-base"
 
-    train_df = pd.read_csv('../data/jigsaw-toxic-comment-train.csv', nrows=16)
-    dev_df = pd.read_csv('../data/validation.csv', nrows=8)
+    train_df = pd.read_csv(
+        '../data/jigsaw-toxic-comment-train.csv', usecols=['comment_text', 'toxic'])
+    train_unintend_bias = pd.read_csv(
+        '../data/jigsaw-unintended-bias-train.csv', usecols=['comment_text', 'toxic'], engine='python', on_bad_lines='skip')
+    train_unintend_bias.toxic = train_unintend_bias.toxic.round().astype(int)
+    n = train_unintend_bias.toxic.sum()
+    train_df = pd.concat([train_df, train_unintend_bias.query(
+        'toxic == 1'), train_unintend_bias.query('toxic == 0').sample(n=n)])
+    dev_df = pd.read_csv('../data/validation.csv',
+                         usecols=['comment_text', 'toxic'])
+    labels = train_df.toxic.to_numpy()
+    weights = class_weight.compute_class_weight(
+        class_weight='balanced',
+        classes=np.unique(labels),
+        y=labels)
+
+    hparams['weights'] = torch.Tensor(weights)
 
     preprocessor = Preprocessor(checkpoint, hparams['token_length'])
     train_X, train_Y = preprocessor(train_df)
